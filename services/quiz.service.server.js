752e4f44cb4964c0672e3792dab21a1d656574b7
wuyushuwys <wuyushuwys@outlook.com>
2020-04-08 18:23:47 -0400

    Add quizzes services

---
+++ services/quiz.service.server.js
@@ -0,0 +1,9 @@
+
+let quizzes = require('./quizzes')
+const findAllQuizzes = () => quizzes
+const findQuizById = (quizId) =>
+    quizzes.find(quiz => quiz._id === quizId)
+module.exports = {
+    findAllQuizzes,
+    findQuizById
+}
---
+++ services/quizzes.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {"_id": "123", "title": "Quiz 1"},
+    {"_id": "234", "title": "Quiz 2"},
+    {"_id": "345", "title": "Quiz 3"}
+]
